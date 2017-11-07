9a3ea2f4abfbf5fed2d9691d373e04427641f4e1
wzhd <hi@wzhd.org>
2017-11-07 15:51:22 +0800

    Write stuctopt help as comments.

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,11 @@
 #[derive(StructOpt, Debug)]
 #[structopt(name = "boatypod", about = "A podcast downloader for newsboat.")]
 pub struct Opt {
-    #[structopt(short = "p", long = "progress", help = "Show progress bar.")]
+    /// Show progress bar.
+    #[structopt(short = "p", long = "progress")]
     pub progress: bool,
 
-    #[structopt(short = "n", long = "num", help = "Limit number of items to download.", default_value = "42")]
+    /// Limit number of items to download.
+    #[structopt(short = "n", long = "num", default_value = "42")]
     pub num: usize,
 }
